0f22cda58e335d9f67ef32b5fa721a2e328d1cbe
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2019-01-05 00:19:40 -0200

    ajustes nas dependencias

--- src/app/shared/shared.module.js
+++ src/app/shared/shared.module.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 
+import 'angular-ui-router';
+import 'oclazyload';
+
 export const SharedModule = angular.module('app.shared', [
     'ui.router',
     'oc.lazyLoad',
--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import * as angular from 'angular';
 
 import '@babel/polyfill';
 
-import 'angular-ui-router';
-import 'oclazyload';
-
 import {AppModule} from './app/app.module';
 
 angular.element(document).ready( ()=> angular.bootstrap(document, [AppModule.name]) );
